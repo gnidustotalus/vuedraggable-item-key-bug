8abc1211d4a41a5c230a5bf9e81886cbea0b9b66
Damian Kalwik <d.kalwik@moonbite.pl>
2021-07-21 10:20:58 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <draggable style="max-width: 400px" v-model="slides" item-key="id">
+    <template #item="{element}">
+      <slide :title="element.title" />
+    </template>
+  </draggable>
+</template>
+
+<script>
+import { reactive, toRefs } from 'vue';
+import draggable from "vuedraggable";
+import Slide from './components/Slide'
+
+export default {
+  name: 'App',
+  components: {
+    draggable,
+    Slide
+  },
+  setup(){
+    const state = reactive({
+      slides: [
+        {
+          title: "Slide1",
+          id: 1,
+        },
+        {
+          title: "Slide2",
+          id: 2
+        }
+      ]
+    })
+
+    return {
+      ...toRefs(state)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Slide.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="slide">{{title}}</div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String
+    },
+    setup(){
+        console.log("Slide::setup()")
+    }
+}
+</script>
+
+<style>
+    .slide {
+        padding: 10px;
+        background-color: royalblue;
+        color: white;
+    }
+</style>
